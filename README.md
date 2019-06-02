abf31c20eaadd7da3345950e60e042faab68dbba
daitianlong <1726713394@qq.com>
2019-06-02 18:07:33 +0800

    集成mybatis

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # daitianlong
 daitianlong
 
-采用的redispool 连接以及连接回收
+1，采用的redispool 连接以及连接回收
 springboot redis demo
+备注：
+a:连接数据库驱动与需要数据库版本基本一致
+b:连接数据库和系统时区差的原因：需要在连接数据库连接上加上：useUnicode=true&amp;characterEncoding=UTF-8&amp;serverTimezone=UTC
---
+++ src/main/java/com/redis/demo/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.redis.demo.mapper;
+
+import com.redis.demo.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
